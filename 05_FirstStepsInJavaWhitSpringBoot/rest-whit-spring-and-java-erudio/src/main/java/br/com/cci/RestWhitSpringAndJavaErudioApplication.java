f01a9c9957fe8badd43c1468db4a63c52d82c2df
Italo Castro <italocastro2010@gmail.com>
2023-03-18 15:55:05 -0300

    First Steps in java With Spring Boot

---
+++ 05_FirstStepsInJavaWhitSpringBoot/rest-whit-spring-and-java-erudio/src/main/java/br/com/cci/RestWhitSpringAndJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.cci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWhitSpringAndJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWhitSpringAndJavaErudioApplication.class, args);
+	}
+
+}
