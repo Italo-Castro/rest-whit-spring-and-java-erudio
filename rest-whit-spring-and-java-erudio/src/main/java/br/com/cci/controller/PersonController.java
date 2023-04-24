1617e368f1a7b18bc56a8bece3e4971723c9e67d
Italo Castro <italocastro2010@gmail.com>
2023-04-24 07:37:26 -0300

    Commit 24/04/2023

---
+++ rest-whit-spring-and-java-erudio/src/main/java/br/com/cci/controller/PersonController.java
@@ -0,0 +1,48 @@
+package br.com.cci.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cci.Service.PersonService;
+import br.com.cci.model.Person;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired 
+	private PersonService service;
+	
+	
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) throws Exception {
+		return service.findById(id);
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) throws Exception {
+		return service.create(person);
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) throws Exception {
+		return service.update(person);
+	}
+	
+}
---
+++ rest-whit-spring-and-java-erudio/src/main/java/br/com/cci/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,46 @@
+package br.com.cci.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.cci.exceptions.ExceptionResponse;
+import br.com.cci.exceptions.ResourceNotFoundException;
+import br.com.cci.exceptions.UnsupportedMathOperationException;
+
+@RestController
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request){ 
+		
+		ExceptionResponse exReponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(exReponse, HttpStatus.INTERNAL_SERVER_ERROR);	
+	}
+
+	
+	@ExceptionHandler(UnsupportedMathOperationException.class)
+	public final ResponseEntity<ExceptionResponse> h(Exception ex, WebRequest request){ 
+	
+		ExceptionResponse exReponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+	
+		return new ResponseEntity<>(exReponse, HttpStatus.BAD_REQUEST);	
+	}
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request){ 
+	
+		ExceptionResponse exReponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+	
+		return new ResponseEntity<>(exReponse, HttpStatus.NOT_FOUND);	
+	}
+	
+}
